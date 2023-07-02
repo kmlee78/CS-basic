e4f3135f35689697833d7814f1da23705c0d987f
kmlee78 <jaoming787@gmail.com>
2023-07-02 11:11:24 +0900

    function, array in c

---
+++ programming/c/array.c
@@ -0,0 +1,42 @@
+#include <studio.h>
+#define NUMBER 5
+
+int main(void)
+{
+    int i, j;
+    // 5개의 정수를 입력받아서 가장 큰 수를 출력하는 프로그램
+    int max, index;
+    int score[NUMBER]; // 크기 5인 배열 선언
+    max = 0;
+    index = 0;
+    for (i = 0; i < NUMBER; i++)
+    {
+        scanf("%d", &score[i]);
+        if (max < score[i])
+        {
+            max = score[i];
+            index = i;
+        }
+    }
+    printf("가장 큰 수는 %d번째 수 %d입니다.\n", index + 1, max);
+
+    // 2차원 배열을 활용하여 5명의 학생의 3과목 점수를 입력받아서 각 학생의 총점을 출력하는 프로그램
+    int students[NUMBER][3];
+    int total[3] = {
+        0,
+    }; // 총점을 저장할 배열, 모든 요소를 0으로 초기화
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf("%d", &students[i][j]);
+            total[i] += students[i][j];
+        }
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d번째 학생의 총점은 %d점입니다.\n", i + 1, total[i]);
+    }
+
+    return 0;
+}
---
+++ programming/c/function.c
@@ -0,0 +1,70 @@
+#include <studio.h>
+
+int hour; // 전역 변수
+int minute;
+int minuteAdd;
+
+void counter()
+{
+    minute += minuteAdd;
+    hour += minute / 60;
+    minute %= 60;
+    hour %= 24;
+}
+
+int getDays(int month, int day)
+{
+    int i, sum = 0;
+    for (i = 1; i < month; i++)
+    {
+        if (i == 2)
+        {
+            sum += 28;
+        }
+        else if (i % 2 == 1)
+        {
+            sum += 31;
+        }
+        else
+        {
+            sum += 30;
+        }
+    }
+    sum += day;
+    return sum;
+}
+
+int combination(int n, int r)
+{
+    if (n == r || r == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return combination(n - 1, r - 1) + combination(n - 1, r);
+    }
+}
+
+int main(void)
+{
+    // 현재 시각에 분을 더하는 프로그램
+    printf("시간, 분, 더할 분을 입력하세요: ");
+    scanf("%d %d %d", &hour, &minute, &minuteAdd);
+    counter();
+    printf("더해진 시간은 %d시 %d분입니다.\n", hour, minute);
+
+    // 월과 일을 입력받아서 1월 1일부터 몇일째인지 계산하는 프로그램
+    int month, day;
+    printf("월과 일을 입력하세요: ");
+    scanf("%d %d", &month, &day);
+    printf("1월 1일부터 %d월 %d일까지는 %d일째 되는 날입니다.\n", month, day, getDays(month, day));
+
+    // 재귀함수를 이용하여 조합을 계산하는 프로그램
+    int n, r;
+    printf("n을 r을 입력하세요: ");
+    scanf("%d %d", &n, &r);
+    printf("%dC%d = %d\n", n, r, combination(n, r));
+
+    return 0;
+}
