5e95755813bc622dce45cc6fb4a5b0f06a2c0664
kmlee78 <jaoming787@gmail.com>
2023-07-07 23:06:13 +0900

    string header file

--- programming/c/string.c
+++ programming/c/string.c
@@ -1,4 +1,5 @@
 #include <studio.h>
+#include <string.h> // 문자열을 다루는 함수들이 선언된 헤더 파일
 
 int main(void)
 {
@@ -12,5 +13,15 @@ int main(void)
     }
     printf("문자열의 길이는 %d입니다.\n", count);
     printf("%s", input); // %s는 문자열을 출력하는 서식 문자
+
+    char str[5] = "abcd";
+    char str2[5] = "efgh";
+    // strlen 함수는 문자열의 길이를 구하는 함수
+    printf("%d", strlen(str));
+    // strcmp 함수는 두 문자열을 비교하는 함수
+    printf("%d", strcmp(str, str2)); // str이 str2보다 사전적으로 앞에 있으면 -1이 출력, str2가 앞에 있으면 1이 출력, 같으면 0이 출력
+    // strcpy 함수는 문자열을 복사하는 함수
+    strcpy(str, str2); // str2를 str에 복사
+    printf("%s", str); // efgh
     return 0;
 }
