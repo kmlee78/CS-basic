b2d83b4a498e5e7f58e1b70828fabe3f543cd661
kmlee78 <jaoming787@gmail.com>
2023-07-08 15:43:27 +0900

    struct advanced

---
+++ programming/c/struct_advanced.c
@@ -0,0 +1,32 @@
+#include <studio.h>
+#include <math.h>
+
+struct point
+{
+    int x;
+    int y;
+}
+
+struct rect
+{
+    struct point p1;
+    struct point p2;
+}
+
+// 두 점을 입력받아서 사각형의 넓이와 둘레를 출력하는 프로그램
+int
+main(void)
+{
+    struct rect r;
+    printf("왼쪽 상단 모서리 x, y 좌표를 입력하세요: ");
+    scanf("%d %d", &r.p1.x, &r.p1.y);
+    printf("오른쪽 하단 모서리 x, y 좌표를 입력하세요: ");
+    scanf("%d %d", &r.p2.x, &r.p2.y);
+    int width = abs(r.p2.x - r.p1.x);
+    int height = abs(r.p2.y - r.p1.y);
+    int area = width * height;
+    int perimeter = 2 * (width + height);
+    printf("넓이: %d\n둘레: %d\n", area, perimeter);
+
+    return 0;
+}
---
+++ programming/c/struct_advanced_2.c
@@ -0,0 +1,30 @@
+#include <studio.h>
+#define SIZE 5
+
+struct student
+{
+    int number;
+    char name[20]; // 한글은 10자까지 가능
+    double grade;
+};
+
+int main(void)
+{
+    struct student list[SIZE]; // 구조체 배열 선언
+    int i;
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("학번을 입력하세요: ");
+        scanf("%d", &list[i].number);
+        printf("이름을 입력하세요: ");
+        scanf("%s", list[i].name);
+        printf("학점을 입력하세요: ");
+        scanf("%lf", &list[i].grade); // double은 %lf로 입력받는다.
+    }
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("학번: %d, 이름: %s, 학점: %.1f\n", list[i].number, list[i].name, list[i].grade);
+    }
+
+    return 0;
+}
